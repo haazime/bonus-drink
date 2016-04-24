2ef26bac60ce83e224c5e6ae6d584ebbdb04d78b
haazime <h4zime@gmail.com>
2016-04-24 14:22:52 +0900

    initial

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    amount + total_bonus_for(amount)
+  end
+
+  def self.total_bonus_for(amount, total = 0)
+    bonus, rest = amount.divmod(3)
+    return total + bonus if (bonus + rest) < 3
+    total_bonus_for(bonus + rest, total + bonus)
   end
 end
