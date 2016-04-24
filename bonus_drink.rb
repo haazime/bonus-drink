381023d91523c8f264f86129b823c5505c125186
haazime <h4zime@gmail.com>
2016-04-24 14:27:58 +0900

    refactor

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,11 +1,13 @@
 class BonusDrink
+  AMOUNT_FOR_BONUS = 3
+
   def self.total_count_for(amount)
     amount + total_bonus_for(amount)
   end
 
   def self.total_bonus_for(amount, total = 0)
-    bonus, rest = amount.divmod(3)
-    return total + bonus if (bonus + rest) < 3
+    bonus, rest = amount.divmod(AMOUNT_FOR_BONUS)
+    return total + bonus if (bonus + rest) < AMOUNT_FOR_BONUS
     total_bonus_for(bonus + rest, total + bonus)
   end
 end
